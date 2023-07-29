4771e93851ae519d5afa5793efa313b2b163482d
nodirbek <nodirbeksamadov666@gmail.com>
2023-07-30 02:03:48 +0500

    final commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,83 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text('welcome', style: TextStyle(fontSize: 20),).tr(),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+                      ),
+                      onPressed: () {
+                        context.setLocale(Locale('en', 'US'));
+                      },
+                      child: Text('English', style: TextStyle(color: Colors.white, fontSize: 15),),
+                    ),
+                  ),
+                  SizedBox(width: 5,),
+                  Expanded(
+                    child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                      ),
+                      onPressed: () {
+                        context.setLocale(Locale('ko', 'KO'));
+                      },
+                      child: Text('Korean', style: TextStyle(color: Colors.white, fontSize: 15),),
+                    ),
+                  ),
+                  SizedBox(width: 5,),
+                  Expanded(
+                    child: TextButton(
+
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                      ),
+                      onPressed: () {
+                        context.setLocale(Locale('ja', 'JA'));
+                      },
+                      child: Text('Japanese', style: TextStyle(color: Colors.white, fontSize: 15),),
+                    ),
+                  ),
+                  // SizedBox(width: 5,),
+                  // Expanded(
+                  //   child: TextButton(
+                  //
+                  //     style: ButtonStyle(
+                  //       backgroundColor: MaterialStateProperty.all<Color>(Colors.orange),
+                  //     ),
+                  //     onPressed: () {
+                  //       context.setLocale(Locale('fr', 'FR'));
+                  //     },
+                  //     child: Text('French', style: TextStyle(color: Colors.white, fontSize: 15),),
+                  //   ),
+                  // ),
+                ],
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
